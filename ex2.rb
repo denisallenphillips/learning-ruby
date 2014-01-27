33d7afbdaffa2224956f66ba8c8ff45ee87ffee3
Denis A Phillips <denisaphillips23@gmail.com>
2014-01-26 20:27:18 -0500

    Exercise 2

---
+++ ex2.rb
@@ -0,0 +1,9 @@
+# buckley is sitting next to me.
+# buckley won't get off the couch.
+
+puts "I could have code like this." # buckley eats a lot.
+
+# buckley loves his hippo.
+puts "this won't run."
+puts "This will run."
+
