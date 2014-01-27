17fa20b5e8fc80813547ccf7ae50707a8b20407c
Denis A Phillips <denisaphillips23@gmail.com>
2014-01-26 20:07:42 -0500

    Exercise 1

---
+++ ex1.rb
@@ -0,0 +1,7 @@
+puts "Hello World!"
+puts "Hello Again."
+puts "I Like Typing This."
+puts "This Is Fun."
+puts 'Yay! Printing.'
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
